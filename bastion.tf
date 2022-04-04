ffea3ada998fcabab8ba7ce0e17e48270cad6564
Alvin Ho <toho113@gmail.com>
2022-04-04 01:32:21 +0000

    recreate using OKE

--- bastion.tf
+++ bastion.tf
@@ -2,7 +2,7 @@ resource "oci_bastion_bastion" "rideb_bastion" {
     #Required
     bastion_type = var.bastion_bastion_type
     compartment_id = var.compartment_ocid
-    target_subnet_id = oci_core_subnet.subnet_2.id  #Private subnet for WEB
+    target_subnet_id = oci_core_subnet.node_subnet.id  #Private subnet for WEB
     client_cidr_block_allow_list = var.bastion_client_cidr_block_allow_list
     name = var.bastion_name
 }
